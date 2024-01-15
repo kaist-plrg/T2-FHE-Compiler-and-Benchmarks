2ce90b4088dcd763a18827fd33a1e1a151d387fc
jaeho <Maokami>
2024-01-15 10:55:32 +0000

    Speed up in building libraries

--- .circleci/build_libs.sh
+++ .circleci/build_libs.sh
@@ -35,7 +35,7 @@ if [ ! -d "OpenFHE/build" ] ; then
     cd ./OpenFHE
     git reset --hard b2869ae
     mkdir -p build && cd build
-    cmake ..
+    cmake -DBUILD_UNITTESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_BENCHMARKS=OFF ..
     make -j 10
     sudo make install
     sudo ln -s /usr/local/lib/libOpenFHEcore.so.1 /usr/lib/libOpenFHEcore.so.1
@@ -50,7 +50,7 @@ echo "Build SEAL v4.1.1"
 if [ ! -d "SEAL/build" ] ; then
     cd ./SEAL
     git reset --hard 206648d0e4634e5c61dcf9370676630268290b59
-    cmake -S . -B build
+    cmake -S . -B build -DSEAL_BUILD_BENCH=OFF -DSEAL_BUILD_EXAMPLES=OFF -DSEAL_BUILD_TESTS=OFF
     cmake --build build
     sudo cmake --install build
     cd ..
